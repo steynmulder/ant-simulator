fc02366659b7fac349fcaf40c079ca2b501a1a93
Steyn Mulder <steynmulder@gmail.com>
2024-04-27 15:35:40 +0200

    Create screen with white background

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ant-simulator
 Project to learn Rust and deepen knowledge on multi-agent systems through simulation of ant-like behavior and generalization to any type of agent
+
+## Dependencies
+This project uses SDL2 release 2.30.2 found at [https://github.com/libsdl-org/SDL/releases/tag/release-2.30.2]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use sdl2::{pixels::Color, event::Event};
+
+fn main() -> Result<(), String>{
+    let (screen_width, screen_height): (u32, u32) = (800, 600);
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let window = video_subsystem.window("Ant Simulator", screen_width, screen_height)
+        .build()
+        .unwrap();
+
+
+    let mut canvas = window.into_canvas()
+        .build()
+        .unwrap();
+    // let screen_area = Rect::new(0, 0, screen_width, screen_height);
+    // let clear_color = Color::RGB(64, 192, 255);
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    let mut running = true;
+    let mut event_queue = sdl_context.event_pump().unwrap();
+    while running {
+
+        for event in event_queue.poll_iter() {
+            match event {
+                Event::Quit {..} => {running = false;},
+                _ => {}
+            }
+        }
+
+        canvas.present();
+    }
+
+
+    Ok(())
 }
